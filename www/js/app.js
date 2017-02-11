1c6d24c698386659cd5e4a1cf637711c79142759
Adil Jad Allah Eddib <jad.eddib@gmail.com>
2017-02-11 18:26:11 +0000

    vouchers

--- www/js/app.js
+++ www/js/app.js
@@ -36,7 +36,13 @@ marketplace.run(function($ionicPlatform, $log, $rootScope, MarketplaceStorage) {
     MarketplaceStorage.executeQuery('INSERT OR IGNORE INTO Users(firsname, lastname, email , username, password, isLoggedIn) VALUES(?, ?, ?, ?, ?, ?)', ["Dan", "Bucholtz", "dan@bucholtz.com", "dan.bucholtz", "password", 0]);
 
     //Vouchers
-    MarketplaceStorage.executeQuery('INSERT OR IGNORE INTO Vouchers(title, reduction, duration, quantity, creator_id) VALUES(?, ?, ?, ?, ?)', ["Adam", "Bradly", 48, 25, 2]);
+    MarketplaceStorage.executeQuery('INSERT OR IGNORE INTO Vouchers(title, shop, reduction, duration, quantity, creator_id) VALUES(?, ?, ?, ?, ?, ?)', ["Get ready for school!", "The cool library", 30, 48, 20, 2]);
+    MarketplaceStorage.executeQuery('INSERT OR IGNORE INTO Vouchers(title, shop, reduction, duration, quantity, creator_id) VALUES(?, ?, ?, ?, ?, ?)', ["Treat yo self this spring!", "Hipster clothing & accessories", 50, 48, 25, 3]);
+    MarketplaceStorage.executeQuery('INSERT OR IGNORE INTO Vouchers(title, shop, reduction, duration, quantity, creator_id) VALUES(?, ?, ?, ?, ?, ?)', ["Eat you weight in Cake!", "Bake n' Cake", 25, 48, 25, 1]);
+    MarketplaceStorage.executeQuery('INSERT OR IGNORE INTO Vouchers(title, shop, reduction, duration, quantity, creator_id) VALUES(?, ?, ?, ?, ?, ?)', ["Get you mom the gift she deserves for Mather's Day", "Jewelery store", 25, 48, 25, 1]);
+    MarketplaceStorage.executeQuery('INSERT OR IGNORE INTO Vouchers(title, shop, reduction, duration, quantity, creator_id) VALUES(?, ?, ?, ?, ?, ?)', ["No outfit is complete without nice shoes", "Shoe Store", 40, 48, 25, 5]);
+    MarketplaceStorage.executeQuery('INSERT OR IGNORE INTO Vouchers(title, shop, reduction, duration, quantity, creator_id) VALUES(?, ?, ?, ?, ?, ?)', ["Get smart with a smart watch", "Gadget store", 35, 48, 25, 4]);
+    MarketplaceStorage.executeQuery('INSERT OR IGNORE INTO Vouchers(title, shop, reduction, duration, quantity, creator_id) VALUES(?, ?, ?, ?, ?, ?)', ["Go see the world like you always dreamed", "Travel agency", 25, 30, 15, 3]);
   })
 })
 .config(function($ionicConfigProvider) {
--- www/js/controllers/client/browseController.js
+++ www/js/controllers/client/browseController.js
@@ -2,7 +2,7 @@
 * @Author: jad
 * @Date:   2017-02-09 10:34:21
 * @Last Modified by:   jad
-* @Last Modified time: 2017-02-11 15:51:45
+* @Last Modified time: 2017-02-11 17:14:57
 */
 
 'use strict';
@@ -14,7 +14,7 @@ controllers.controller("BrowseController", function($scope, $state, $ionicSideMe
 		$rootScope.showMenuIcon = true;
 		$scope.data = {};
 		var user = ObjectService.getUser();
-		var selectAllVouchers = "SELECT * FROM vouchers WHERE creator_id != ? ORDER BY idVoucher DESC";
+		var selectAllVouchers = "SELECT title, store, quantity, duration, creator_id, firstname, lastname, idUser FROM Vouchers, Users WHERE creator_id != ? AND creator_id = idUser ORDER BY idVoucher DESC";
 		MarketplaceStorage.executeQuery(selectAllVouchers, [user.idUser]).then(function(res) {
 			for (var i = 0; i < res.rows.length; i++) {
 				$scope.data.vouchers.push(res.rows.item(i));
