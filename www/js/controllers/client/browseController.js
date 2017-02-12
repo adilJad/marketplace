217cc1cfcdb19ef21ed0d1dcfc0520ee85f0e830
Adil Jad Allah Eddib <jad.eddib@gmail.com>
2017-02-12 12:01:10 +0000

    get voucher

--- www/js/controllers/client/browseController.js
+++ www/js/controllers/client/browseController.js
@@ -2,29 +2,52 @@
 * @Author: jad
 * @Date:   2017-02-09 10:34:21
 * @Last Modified by:   jad
-* @Last Modified time: 2017-02-12 10:20:26
+* @Last Modified time: 2017-02-12 11:59:06
 */
 
 'use strict';
 
-controllers.controller("BrowseController", function($scope, $state, $ionicSideMenuDelegate, $rootScope, $log, MarketplaceStorage, ObjectService, $stateParams) {
+controllers.controller("BrowseController", function($scope, $state, $ionicSideMenuDelegate, $rootScope, $log, MarketplaceStorage, ObjectService, $stateParams, $ionicPopup) {
 	$scope.$on('$ionicView.beforeEnter', function(event, config) {
 		config.enableBack = false;
 		$ionicSideMenuDelegate.canDragContent(true);
 		$rootScope.showMenuIcon = true;
 
 		MarketplaceStorage.executeQuery("SELECT * FROM Users WHERE isLoggedIn = 1").then(function(res) {
-			var user = res.rows.item(0);
+			$scope.data = {};
+			$scope.data.user = res.rows.item(0);
 			var selectAllVouchers = "SELECT title, shop, reduction, quantity, duration, creator_id, firstname, lastname, idUser FROM Vouchers, Users WHERE creator_id != ? AND creator_id = idUser ORDER BY idVoucher DESC";
-			MarketplaceStorage.executeQuery(selectAllVouchers, [user.idUser]).then(function(res) {
-				$scope.data = {
-					vouchers: []
-				};
+			MarketplaceStorage.executeQuery(selectAllVouchers, [$scope.data.user.idUser]).then(function(res) {
+				$scope.data.vouchers = [];
 				for (var i = 0; i < res.rows.length; i++) {
 					$scope.data.vouchers.push(res.rows.item(i));
 				}
 			});
 		})
 	});
+
+	$scope.requestVoucher = function(i) {
+		$scope.confirmRequest = $ionicPopup.confirm({
+	       title: 'Claim this Voucher',
+	       template: 'Are you sure you want this Voucher?'
+     	});
+		$scope.confirmRequest.then(function(res) {
+			if(res) {
+				MarketplaceStorage.executeQuery("SELECT COUNT(*) AS c FROM Users_Vouchers WHERE users_idUser = ? AND vouchers_idVoucher = ? ", [$scope.data.user.idUser, $scope.data.vouchers[i].idVoucher]).then(function(res) {
+					if(res.rows.item(0).c == 0) {
+						MarketplaceStorage.executeQuery("INSERT INTO Users_Vouchers(users_idUser, vouchers_idVoucher) VALUES(?, ?)", [$scope.data.user.idUser, $scope.data.vouchers[i].idVoucher]).then(function(res) {
+							var q = $scope.data.vouchers[i].quantity - 1;
+							MarketplaceStorage.executeQuery("UPDATE Vouchers SET quantity=?", q);
+						});
+					} else {
+						$cordovaToast.show("You already have this voucher", 'long', 'bottom');
+					}
+				});
+			} else {
+				console.log('You are not sure');
+			}
+		});
+	};
+
 
 })
--- www/js/controllers/signupController.js
+++ www/js/controllers/signupController.js
@@ -2,25 +2,35 @@
 * @Author: jad
 * @Date:   2017-02-09 11:10:13
 * @Last Modified by:   jad
-* @Last Modified time: 2017-02-12 10:24:54
+* @Last Modified time: 2017-02-12 11:09:11
 */
 
 'use strict';
 
-controllers.controller("SignupController", function($scope, $state, $log, $ionicSideMenuDelegate, $rootScope, $cordovaSQLite, $cordovaToast) {
+controllers.controller("SignupController", function($scope, $state, $log, $ionicSideMenuDelegate, $rootScope, $cordovaToast, MarketplaceStorage, $ionicPopup) {
 	$scope.$on('$ionicView.enter', function(e) {
 		$ionicSideMenuDelegate.canDragContent(false);
 		$rootScope.showMenuIcon = false;
 	});
 
 	$scope.signupData = {};
+	$scope.signupData.password = "";
+	$scope.signupData.firstName = "";
+	$scope.signupData.lastName = "";
+	$scope.signupData.email = "";
+	$scope.signupData.username = "";
 
 	$scope.signup = function() {
 		$log.debug($scope.signupData);
-		MarketplaceStorage.executeQuery('INSERT OR IGNORE INTO Users(firstname, lastname, email , username, password, isLoggedIn) VALUES(?, ?, ?, ?, ?, ?)', [$scope.signupData.fisrtName, $scope.signupData.lastName, $scope.signupData.email, $scope.signupData.username, $scope.signupData.password, 0]).then(function() {
-			$cordovaToast.show("user created successfully! please log in", 'long', 'bottom');
-		});
-		
-
+		if($scope.signupData.password == "" || $scope.signupData.firstName == "" || $scope.signupData.lastName == "" ||  $scope.signupData.email == "" || $scope.signupData.username == ""){
+			$cordovaToast.show("Sign up form is incomplete", 'long', 'bottom');
+		} else {
+			MarketplaceStorage.executeQuery('INSERT INTO Users(firstname, lastname, email , username, password, isLoggedIn) VALUES(?, ?, ?, ?, ?, ?)', [$scope.signupData.fisrtName, $scope.signupData.lastName, $scope.signupData.email, $scope.signupData.username, $scope.signupData.password, 0]).then(function() {
+				$cordovaToast.show("user created successfully! please log in", 'long', 'bottom');
+				$state.go("app.login");
+			}, function(err) {
+				$cordovaToast.show("this username is taken", 'long', 'bottom');
+			});
+		}
 	}
 })
